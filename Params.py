ef8f1062c748302ac977c4fc36a057ca4729f660
BarryShaffer <barry.shaffer@gmail.com>
2023-03-29 08:48:02 -0700

    Create Params.py

---
+++ Params.py
@@ -0,0 +1,35 @@
+""" the basic parameter set for Omnirig CAT Control"""
+
+import win32com.client
+
+PM_UNKNOWN =    1
+PM_FREQ =       2
+PM_FREQA =      4
+PM_FREQB =      8
+PM_PITCH =      16
+PM_RITOFFSET =  32
+PM_RIT0 =       64
+PM_VFOAA =      128
+PM_VFOAB =      256
+PM_VFOBA =      512
+PM_VFOBB =      1024
+PM_VFOA =       2048
+PM_VFOB =       4096
+PM_VFOEQUAL =   8192
+PM_VFOSWAP =    16384
+PM_SPLITON =    32768
+PM_SPLITOFF =   65536
+PM_RITON =      131072
+PM_RITOFF =     262144
+PM_XITON =      524288
+PM_XITOFF =     1048576
+PM_RX =         2097152
+PM_TX =         4194304
+PM_CW_U =       8388608
+PM_CW_L =       16777216
+PM_SSB_U =      33554432
+PM_SSB_L =      67108864
+PM_DIG_U =      134217728
+PM_DIG_L =      268435456
+PM_AM =         536870912
+PM_FM =         1073741824
